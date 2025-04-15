9279f56ebfd8a5a1233805940436bb4e4bdb4584
Teja <97877709+Codewithtej@users.noreply.github.com>
2025-04-14 20:12:42 -0700

    Update trail_app.py

--- trail_app.py
+++ trail_app.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
 import streamlit as st
-api_key = st.secrets["openai_api_key"]
+open_api_key = st.secrets["openai_api_key"]
 
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key=open_api_key)
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
